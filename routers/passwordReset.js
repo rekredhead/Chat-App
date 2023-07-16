69118fed9ea973d87b46391138c47d971613484f
rekredhead <rekredhead@gmail.com>
2023-07-17 03:25:08 +0530

    Able to successfully change user's password through verification

--- routers/passwordReset.js
+++ routers/passwordReset.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dbConnection = require('../database/dbConnection');
 const isVerificationEmailSent = require('./functions/sendVerificationCodeToEmail');
+const encryptPassword = require('./functions/encryptPassword');
 const { cachePasswordResettingData, getPasswordResettingDataFromCache } = require('./functions/handleRedisCaching');
 const router = express.Router();
 
@@ -21,7 +22,7 @@ const generateRandomCode = () => {
 router.post('/users/password-reset/new-password', (req, res) => {
    const { username, newPassword } = req.body;
 
-   const findUsernameInDBQuery = `SELECT username,email FROM USER WHERE username='${username}'`;
+   const findUsernameInDBQuery = `SELECT email FROM USER WHERE username='${username}'`;
    dbConnection.query(findUsernameInDBQuery, async(err, result) => {
       if (err) throw err;
       if (result.length === 0) {
@@ -29,10 +30,9 @@ router.post('/users/password-reset/new-password', (req, res) => {
          return;
       }
 
-      const verificationCode = generateRandomCode();
+      const verificationCode = generateRandomCode(username);
       await cachePasswordResettingData(username, newPassword, verificationCode);
 
-      // Send verification code to user email
       const email = result[0].email;
       if (!isVerificationEmailSent(email, verificationCode)) {
          res.status(400).send({ message: "Error with sending verification code. Please try again"});
@@ -44,10 +44,41 @@ router.post('/users/password-reset/new-password', (req, res) => {
 });
 
 router.post('/users/password-reset/verification', (req, res) => {
-   // Get userID from session, verificationCode from req.body
-   // Check if userID is cached in Redis - send error if not
-   // Check if verificationCode matches cached verificationCode
-   // Update the new password of user in database
+   const { username, verificationCode } = req.body;
+
+   const findUsernameInDBQuery = `SELECT email FROM USER WHERE username='${username}'`;
+   dbConnection.query(findUsernameInDBQuery, async(err, result) => {
+      if (err) throw err;
+      if (result.length === 0) {
+         res.status(400).send({ message: "Username not found" });
+         return;
+      }
+
+      const cachedPasswordResetData = await getPasswordResettingDataFromCache(username);
+
+      const isUsernameInCachedData = Object.entries(cachedPasswordResetData).length > 0;
+      if (!isUsernameInCachedData) {
+         res.status(400).send({ message: "This verification code has expired. Please try again" });
+         return;
+      }
+
+      const isVerificationCodeCorrect = cachedPasswordResetData.verificationCode === verificationCode;
+      if (!isVerificationCodeCorrect) {
+         res.status(400).send({ message: "Incorrect verification code" });
+         return;
+      }
+
+      // Update password for the user in the database
+      const newEncryptedPassword = await encryptPassword(cachedPasswordResetData.password);
+      const updateUserPasswordQuery = `UPDATE USER SET password='${newEncryptedPassword}' WHERE username='${username}'`;
+      dbConnection.query(updateUserPasswordQuery, (err) => {
+         if (err) throw err;
+         res.status(200).send({ message: "Password changed succesfully" });
+      });
+   });
+   // Remove password and confirm password inputs in frontend after user presses change password button
+   // Replace them with the verificationCode input and start a 5 minute countdown when a OK message is received
+   // Put the password and confirm password inputs again after the countdown - store the countdown in localstorage so users can't refresh the page to clear it
 });
 
 module.exports = router;
