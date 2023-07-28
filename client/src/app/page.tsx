a49056b1206a2666ab73003d0b3bf1a0a3b3dcd0
rekredhead <rekredhead@gmail.com>
2023-07-28 11:07:22 +0530

    Enabled basic chatting in chat page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import io, { Socket } from 'socket.io-client';
 import Link from "next/link";
 import Image from "next/image";
@@ -8,6 +8,12 @@ import 'material-icons/iconfont/material-icons.css';
 interface messagesData {
    id: string;
    text: string;
+   type: string;
+}
+interface messageProps {
+   type: string;
+   text: string;
+   index: number;
 }
 
 function Header({ onClick }: any) {
@@ -41,15 +47,14 @@ function Header({ onClick }: any) {
       </header>
    );
 }
-
 function NavBar({ isMenuExpanded }: any) {
    return (
       <nav className={`flex flex-col items-center mt-16 py-2 px-3 gap-2 bg-slate-900 w-auto overflow-y-scroll`}>
          {
             [0, 1, 2, 3, 4].map((item, index) => {
                return (
-                  <div className="flex items-center gap-2">
-                     <div key={index} className="h-10 w-10 rounded-full overflow-hidden">
+                  <div key={index} className="flex items-center gap-2">
+                     <div className="h-10 w-10 rounded-full overflow-hidden">
                         <Image
                            src="/kehan.png"
                            alt="Kehan PFP"
@@ -67,15 +72,31 @@ function NavBar({ isMenuExpanded }: any) {
       </nav>
    );
 }
+function Message({ type, text, index }: messageProps) {
+   switch (type) {
+      case 'self-message':
+         return <p key={index} className="p-2 rounded-md text-lg w-fit bg-blue-600 self-end">{text}</p>;
+      case 'other-message':
+         return <p key={index} className="p-2 rounded-md text-lg w-fit bg-green-600 self-start">{text}</p>;
+      case 'notification':
+         return <p key={index} className="p-2 rounded-md text-lg w-fit bg-orange-600 self-center">{text}</p>;
+   }
+}
 
 export default function Home() {
    const SERVER_DOMAIN = process.env.SERVER_DOMAIN;
 
+   const messageContainerRef = useRef<HTMLDivElement>(null);
    const [isMenuExpanded, setIsMenuExpanded] = useState(false);
    const [messages, setMessages] = useState<messagesData[]>([]);
    const [socket, setSocket] = useState<Socket | null>(null);
 
    const toggleMenu = () => setIsMenuExpanded(prevState => !prevState);
+
+   // Scroll to the bottom of message container whenever messages change
+   useEffect(() => {
+      messageContainerRef.current?.scrollTo(0, messageContainerRef.current.scrollHeight);
+   }, [messages]);
 
    // Only establish the websocket connection once after the page load
    useEffect(() => {
@@ -91,8 +112,25 @@ export default function Home() {
    // Run this everytime a change occurs to the socket 
    useEffect(() => {
       if (socket) {
+         // Cache the user's websocket id
+         if (socket) socket.on('id', (id) => localStorage.setItem('id', id));
+
          socket.on('message', (message) => {
-            const data = JSON.parse(message);
+            const { id, text } = JSON.parse(message);
+            const data = {
+               id,
+               text,
+               type: id === localStorage.getItem('id') ? 'self-message' : 'other-message'
+            }
+            setMessages((prevMessages) => [...prevMessages, data]);
+         });
+
+         socket.on('notification', (id) => {
+            const data = {
+               id,
+               text: id === localStorage.getItem('id') ? 'You joined this chat' : `${id} joined this chat`,
+               type: 'notification'
+            }
             setMessages((prevMessages) => [...prevMessages, data]);
          });
       }
@@ -103,11 +141,14 @@ export default function Home() {
          alert("Socket is null. Cannot send the message");
          return;
       }
-      
-      const text = data.get('myMessage')?.valueOf().toString().trim();
+
+      const text = data.get('text')?.valueOf().toString().trim();
       if (!text) return;
 
       socket.emit('message', text);
+
+      const inputElement = document.querySelector<HTMLInputElement>('input[name="text"]');
+      if (inputElement) inputElement.value = '';
    }
 
    return (
@@ -116,10 +157,11 @@ export default function Home() {
          <div className="double-grid w-full h-full">
             <NavBar isMenuExpanded={isMenuExpanded} />
             <main className="flex relative mt-16">
-               <div className="flex flex-col absolute top-0 left-0 right-0 bottom-24 p-5 gap-2 border">
-                  <p className="p-2 rounded-md text-lg w-fit bg-blue-600 self-end">Hi</p>
-                  <p className="p-2 rounded-md text-lg w-fit bg-green-600 self-start">Hello</p>
-                  <p className="p-2 rounded-md text-lg w-fit bg-orange-600 self-center">Notification 1</p>
+               <div
+                  ref={messageContainerRef}
+                  className="flex flex-col absolute top-0 left-0 right-0 bottom-24 p-5 gap-2 overflow-hidden overflow-y-scroll"
+               >
+                  { messages.map((data, index) => <Message type={data.type} text={data.text} index={index} />) }
                </div>
                <form className="flex absolute justify-center py-2 bottom-8 left-0 right-0 gap-2" action={handleSubmit}>
                   <input
